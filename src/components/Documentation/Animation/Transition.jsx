49b112f7360c8896a58df88986cd92f348349ab8
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-03-11 10:42:29 +0330

    work on Transition section

--- src/components/Documentation/Animation/Transition.jsx
+++ src/components/Documentation/Animation/Transition.jsx
@@ -6,12 +6,12 @@ const Transition = () => {
     show: {
       opacity: 1,
       transition: {
-        delay:2,
-        duration:1,
+        delay: 2,
+        duration: 1,
         delayChildren: 1.5,
         staggerChildren: 1.5,
         staggerDirection: -1,
-        when:"beforeChildren"
+        when: "beforeChildren",
       },
     },
   };
@@ -22,10 +22,31 @@ const Transition = () => {
   };
   return (
     <div>
-      <motion.ul style={{backgroundColor:"red"}} variants={container} initial="hidden" animate="show">
+      <motion.ul
+        style={{ backgroundColor: "red" }}
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
         <motion.li variants={item} />
         <motion.li variants={item} />
       </motion.ul>
+      <motion.div
+        className="box-container"
+        animate={{ rotate: 180 }}
+        transition={{
+          repeat: 1,
+          repeatType: "reverse",
+          repeatDelay: 1,
+          duration: 2,
+          from: 270,
+        }}
+      />
+      <motion.div
+        className="box-container"
+        animate={{ scale: [0, 1, 0.5, 1] }}
+        transition={{duration:4, times: [0, 0.3, 0.9, 1] }}
+      />
     </div>
   );
 };
