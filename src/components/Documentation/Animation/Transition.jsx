12a771c00cc05e4aea62a0966a859b30db617fff
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-03-11 09:43:22 +0330

    work on Transition section

--- src/components/Documentation/Animation/Transition.jsx
+++ src/components/Documentation/Animation/Transition.jsx
@@ -1,5 +1,33 @@
+import { motion } from "framer-motion";
+
 const Transition = () => {
-  return <div></div>;
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delay:2,
+        duration:1,
+        delayChildren: 1.5,
+        staggerChildren: 1.5,
+        staggerDirection: -1,
+        when:"beforeChildren"
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
+  return (
+    <div>
+      <motion.ul style={{backgroundColor:"red"}} variants={container} initial="hidden" animate="show">
+        <motion.li variants={item} />
+        <motion.li variants={item} />
+      </motion.ul>
+    </div>
+  );
 };
 
 export default Transition;
